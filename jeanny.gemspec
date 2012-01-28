d4d3184c61f62fcc7fa8c6819349405c196d2cb9
Gun.io Whitespace Robot <contact@gun.io>
2012-01-28 02:15:34 -0500

    Remove whitespace [WhitespaceBot]

--- jeanny.gemspec
+++ jeanny.gemspec
@@ -1,17 +1,17 @@
 spec = Gem::Specification.new do |gem|
-    
+
     gem.name            = 'jeanny'
     gem.version         = '0.99.7'
     gem.summary         = 'Lib for obfuscation css class names'
     # gem.description     = 'Lib for obfuscation css class names'
     gem.files           = ['README.md', 'lib/jeanny.rb', 'lib/jeanny/engine.rb', 'lib/jeanny/sugar.rb', 'lib/jeanny/extend.rb']
     gem.require_path    = 'lib'
-    
+
     gem.has_rdoc        = true
     gem.rdoc_options    = ['--inline-source', '--charset=UTF-8']
-    
+
     gem.author          = 'gfranco'
     gem.email           = 'hello@gfranco.ru'
     gem.homepage        = 'http://github.com/gfranco/jeanny'
-    
+
 end
--- lib/jeanny/sugar.rb
+++ lib/jeanny/sugar.rb
@@ -19,40 +19,40 @@ module Jeanny
                 refresh
 
                 begin
-                    
+
                     file_list = File.list path
-                    
+
                     fail JeannyFileNotFound, "файлы для анализа не найдены (Jeanny::Engine.analyze)" if file_list.empty?
-                    
+
                     file_meat = ''
                     file_list.each do |file|
                         file_meat = file_meat + File.open_file(file)
                     end
-                    
+
                     if @engine.analyze file_meat
                         @canbe[:save], @canbe[:process] = true, true
                         @canbe[:analyze] = false
                     end
-                    
+
                 rescue StandardError => e
                     $stderr.puts "Ошибка: ".red + e.message
                     exit 1
                 end
-                
+
                 @compare_file = (args[:compare_with] or '')
-                
+
                 # Завершаем метод если сравнивать ни с чем не надо
                 return true if @compare_file.empty?
-                
+
                 # Если файл не найден, спрашиваем у юзернейма, как быть дальше,
                 # продолжать без сравнения или прекратить работу.
                 unless File.exists? @compare_file
                     puts "Файл с сохраненными классами не найден. Продолжаем без сравнения.".yellow
                     return true
                 end
-                
+
                 saved_classes = []
-                
+
                 begin
                     saved_classes = YAML.load_file @compare_file
                     saved_classes.map! do |x|
@@ -60,12 +60,12 @@ module Jeanny
                     end
                 rescue Exception => e
                     $stderr.puts e.message
-                    exit 1                    
+                    exit 1
                 end
-                
+
                 # Сравниваем
                 new_classes = @engine.compare_with saved_classes
-                
+
                 unless new_classes.nil? or new_classes.empty?
                     puts 'Новые классы: '
                     new_classes.each do |class_name|
@@ -74,11 +74,11 @@ module Jeanny
                 end
 
                 true
-                
+
             end
 
             def save file = ''
-                
+
                 fail RuntimeError, 'на данном этапе нельзя вызывать сохранение классов' unless @canbe[:save]
 
                 file = file.empty? ? @compare_file : file
@@ -92,15 +92,15 @@ module Jeanny
             def save_to file
                 save file
             end
-            
+
             def group type, args = {}, &block
-   
+
                 begin
-                    
+
                     fail "We can`t process here..." unless @canbe[:process]
                     fail "Блоки process не должны быть рекурсивными" if @process_block_start
                     fail "Не передан блок" unless block_given?
-                    
+
                     @canbe[:process] = false
                     @canbe[:replace] = true
 
@@ -108,12 +108,12 @@ module Jeanny
                     @process_block = []
 
                     yield block
-                    
+
                     @process_block_start = false
 
                     @canbe[:replace] = false
                     @canbe[:process] = true
-                    
+
                 rescue Exception => e
                     $stderr.puts "Ошибка: ".red + e.message
                     exit 1
@@ -121,13 +121,13 @@ module Jeanny
 
                 @process_block.each do |replace|
                     File.list replace[:in] do |file, status|
-                        
+
                         # next unless status.zero?
                         unless status.zero?
                             puts file.red
                             next
                         end
-                        
+
                         exclude = false
                         replace[:ex].each do |exclude_rule|
                             if exclude_rule.kind_of? Regexp
@@ -137,35 +137,35 @@ module Jeanny
                             end
                             break if exclude
                         end
-                        
+
                         if exclude
                             puts file.yellow
                             next
                         end
-                        
+
                         begin
-                            
+
                             puts file.green
 
                             data = File.open_file file
                             data = @engine.replace data, type
 
                             File.save_file file, data, replace[:prefix]
-                            
+
                         rescue Exception => e
                             puts e.message + "\n#{$@}"
                             exit 1
                         end
-                        
+
                     end
                 end
-                
+
             end
-            
+
             def replace args = { }
-                
+
                 fail "We can`t replace here..." unless @canbe[:replace]
-                
+
                 struct = { :in => [], :ex => [], :prefix => '' }
 
                 struct[:in] = ([args[:in]] | [args[:include]]).flatten.delete_if { |item| item.nil? or item.empty? }
@@ -174,7 +174,7 @@ module Jeanny
                 struct[:prefix] = args[:prefix] unless args[:prefix].nil?
 
                 @process_block << struct if struct[:in].length > 0
-                
+
             end
 
             private
@@ -194,24 +194,24 @@ module Jeanny
 
                 @process_block_start = false
                 @process_block = []
-                
+
             end
 
             def answer question, yes, no
-                
+
                 action, answers = '', [yes, no].flatten
                 until answers.include? action
                     print "#{question} "
                     action = gets.chomp!
                 end
-                    
+
                 [yes].flatten.include? action
-                
+
             end
 
         end
 
-        # Тут перехватываем все несуществующие методы. 
+        # Тут перехватываем все несуществующие методы.
         # И те которые используются в DSL отправляем куда надо.
         def method_missing(method, *args, &block)
             BridgeToJeanny.instance.send(method, *args, &block) if BridgeToJeanny.instance.respond_to?(method)
